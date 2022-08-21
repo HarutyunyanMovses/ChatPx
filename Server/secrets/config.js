e038a12b30baf414c0544e0cd1f408dc5a1ba3fc
HarutyunyanMovses <107640630+HarutyunyanMovses@users.noreply.github.com>
2022-08-22 02:54:30 +0400

    Delete Server/secrets directory

--- Server/secrets/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = { 
-    secret : 'JWT_KEY',
-    MONGO_URL:"mongodb+srv://admin:admin@cluster0.tcjaeiy.mongodb.net/Chat", //retryWrites=true&w=majority
-    PORT : 3030,
-    serverUrl: "http://localhost:3030/chatpx/files/"
-}
